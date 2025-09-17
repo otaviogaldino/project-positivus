b87f0cf54148b56a785e3dd5de75c47a6f067439
otavio <suaresotavio@gmail.com>
2025-09-17 16:06:48 -0300

    feat: adicionar cartões de processo com interatividade para expandir e recolher respostas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,24 @@
-let processCard = document.querySelector('.process-card');
-let iconMais = document.querySelector('.icon-mais');
-let iconMenos = document.querySelector('.icon-menos');
-let processCardResposta = document.querySelector('.process-card-resposta');
+let processCards = document.querySelectorAll('.process-card');
 
-iconMais.addEventListener('click', () => {
-    processCardResposta.style.display = 'block';
-    iconMais.style.display = 'none';
-    iconMenos.style.display = 'block';
-    processCard.style.backgroundColor = '#B9FF66';
-    processCard.style.transition = '0.3s';
-})
-iconMenos.addEventListener('click', () => {
-    processCardResposta.style.display = 'none';
-    iconMais.style.display = 'block';
-    iconMenos.style.display = 'none';
-    processCard.style.backgroundColor = '#F3F3F3';
-    processCard.style.transition = '0.3s';
+processCards.forEach(card => {
+    let iconMais = card.querySelector('.icon-mais');
+    let iconMenos = card.querySelector('.icon-menos');
+    let processCardResposta = card.querySelector('.process-card-resposta');
+
+    
+
+    iconMais.addEventListener('click', () => {
+        processCardResposta.style.display = 'block';
+        iconMais.style.display = 'none';
+        iconMenos.style.display = 'block';
+        card.style.backgroundColor = '#B9FF66';
+        card.style.transition = '0.3s';
+    })
+    iconMenos.addEventListener('click', () => {
+        processCardResposta.style.display = 'none';
+        iconMais.style.display = 'block';
+        iconMenos.style.display = 'none';
+        card.style.backgroundColor = '#F3F3F3';
+        card.style.transition = '0.3s';
+    })
 })
